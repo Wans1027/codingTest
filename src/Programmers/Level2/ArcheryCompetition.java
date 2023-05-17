2e2648340bb12fbfcb335ed7c5b4539899d3837f
wan1027 <rladhkstmd10@daum.net>
2023-05-17 18:14:06 +0900

    level2

---
+++ src/Programmers/Level2/ArcheryCompetition.java
@@ -0,0 +1,19 @@
+package Programmers.Level2;
+
+import java.util.Scanner;
+
+public class ArcheryCompetition {
+    public static void main(String[] args) {
+
+    }
+    //완전탐색으로 풀어야 하나>????
+    public static int[] solution(int n, int[] info) {
+        int[] answer = {};
+        // n 화살의 수
+        int lionArrow = n;
+        for (int i = 0; i <= 10; i++) {
+
+        }
+        return answer;
+    }
+}
---
+++ src/Programmers/Level2/NQeen.java
@@ -0,0 +1,17 @@
+package Programmers.Level2;
+
+import java.util.Scanner;
+
+public class NQeen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[][] map = new int[n][n];
+
+        queen();
+    }
+
+    private static void queen(int x, int y) {
+
+    }
+}
