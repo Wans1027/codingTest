7d0a2a9e846e329320ac7c0a0f92c061d02b1488
Wan1027 <rladhkstmd10@daum.net>
2023-05-22 04:21:00 +0900

    baekJoon start

---
+++ src/Programmers/Test.java
@@ -0,0 +1,12 @@
+package Programmers;
+
+import java.util.Arrays;
+
+public class Test {
+    public String solution(String sentence) {
+        String answer = "";
+        char[] chars = sentence.toCharArray();
+
+        return answer;
+    }
+}
