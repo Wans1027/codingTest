543a69181a2fdfe620c93df0eeeca8ee720c32b0
wan1027 <rladhkstmd10@daum.net>
2023-06-27 19:49:22 +0900

    06/27

---
+++ src/Programmers/Level2/Kaffet.java
@@ -0,0 +1,17 @@
+package Programmers.Level2;
+
+import java.util.Scanner;
+
+public class Kaffet {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int brown = sc.nextInt();
+        int yellow = sc.nextInt();
+
+        //b+y = S = w * h
+        // y = w-2 * h-2
+        // b = S - y;
+        // b = w * h -( w-2 * h-2 ) =
+
+    }
+}
