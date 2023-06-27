fd71c75f9076278ded877bc36718fa64cea4b09c
wan1027 <rladhkstmd10@daum.net>
2023-06-27 20:01:52 +0900

    06/27

--- src/Programmers/Level2/Kaffet.java
+++ src/Programmers/Level2/Kaffet.java
@@ -11,7 +11,29 @@ public class Kaffet {
         //b+y = S = w * h
         // y = w-2 * h-2
         // b = S - y;
-        // b = w * h -( w-2 * h-2 ) =
 
+
+    }
+
+    public int[] solution(int brown, int yellow) {
+
+        int[] answer = new int[2];
+
+        int s = brown + yellow;
+
+        for (int i = 3; i < s; i++) {
+            int j = s/i;
+            if (s % i == 0 && j >= 3) {
+                int col = Math.max(i, j);
+                int row = Math.min(i, j);
+                int center = (col-2) * (row-2);
+                if (center == yellow) {
+                    answer = new int[]{col, row};
+                    return answer;
+                }
+            }
+        }
+
+        return answer;
     }
 }
