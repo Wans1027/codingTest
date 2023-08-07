70a9b4ae2ca2074233e488e9040b33988a8f6360
Wan1027 <rladhkstmd10@daum.net>
2023-08-08 00:16:32 +0900

    0808

--- src/Programmers/Level3/BaloonBombDFS.java
+++ src/Programmers/Level3/BaloonBombDFS.java
@@ -53,11 +53,25 @@ public class BaloonBombDFS {
         else return loc;
     }
 }
-public class BalloonBombCollectAnswer {
-    public static void main(String[] args) {
-    }
-    public int solution(int[] a) {
-        int answer = 0;
+class BalloonBombCollectAnswer {
+    public static int solution(int[] a) {
+        int answer = 2;
+        if(a.length == 1) return 1;
+
+        int leftMin = a[0];
+        int[] rightMin = new int[a.length];
+        rightMin[a.length-1] = a[a.length-1];
+
+        for (int i = a.length-2; i > 0; i--) {
+            rightMin[i] = Math.min(rightMin[i+1], a[i]);
+        }
+
+        for (int i = 1; i < a.length-1; i++) {
+            if(!(a[i]>leftMin && a[i]> rightMin[i])){
+                answer++;
+            }
+            leftMin = Math.min(leftMin, a[i]);
+        }
         return answer;
     }
 }
